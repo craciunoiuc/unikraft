c3f524837629a60f1272f206bb6ecd794f509e43
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2021-04-09 14:33:08 +0300

    Change types to match libparams

--- lib/ukstore/include/uk/store/store.h
+++ lib/ukstore/include/uk/store/store.h
@@ -35,101 +35,77 @@
 
 #include <uk/store/tree.h>
 
-// TODO Save uk_tree_node-s in the section insteam of uk_store_entry-s?
+// TODO Save uk_tree_node-s in the section instead of uk_store_entry-s?
 // TODO refcount -> hash path into integer and store it?
+// TODO add name of entry to entry
+// TODO Reduce implementation to bare minimum
 
 /* All types used by the structure */
 enum uk_store_entry_type {
-	UK_STORE_NONE,
-	UK_STORE_ENT_BOOL,
+	UK_STORE_ENT_NONE,
+	UK_STORE_ENT_S8,
+	UK_STORE_ENT_U8,
+	UK_STORE_ENT_S16,
+	UK_STORE_ENT_U16,
+	UK_STORE_ENT_S32,
+	UK_STORE_ENT_U32,
+	UK_STORE_ENT_S64,
+	UK_STORE_ENT_U64,
+	UK_STORE_ENT_UPTR,
 	UK_STORE_ENT_INT,
-	UK_STORE_ENT_SHORT,
 	UK_STORE_ENT_CHAR,
-	UK_STORE_ENT_LONG,
-	UK_STORE_ENT_LONGLONG,
-	UK_STORE_ENT_FLOAT,
-	UK_STORE_ENT_DOUBLE,
-	UK_STORE_ENT_UINT,
-	UK_STORE_ENT_USHORT,
-	UK_STORE_ENT_UCHAR,
-	UK_STORE_ENT_ULONG,
-	UK_STORE_ENT_ULONGLONG,
-	UK_STORE_ENT_VOID,
-	UK_STORE_ENT_VOIDSTAR,
+	UK_STORE_ENT_CHARP,
 };
 
 /* Getter definitions */
-typedef _Bool (*uk_store_get_bool_func_t)(void);
-typedef int (*uk_store_get_int_func_t)(void);
-typedef short (*uk_store_get_short_func_t)(void);
-typedef char (*uk_store_get_char_func_t)(void);
-typedef long (*uk_store_get_long_func_t)(void);
-typedef long long (*uk_store_get_longlong_func_t)(void);
-typedef float (*uk_store_get_float_func_t)(void);
-typedef double (*uk_store_get_double_func_t)(void);
-typedef unsigned int (*uk_store_get_uint_func_t)(void);
-typedef unsigned short (*uk_store_get_ushort_func_t)(void);
-typedef unsigned char (*uk_store_get_uchar_func_t)(void);
-typedef unsigned long (*uk_store_get_ulong_func_t)(void);
-typedef unsigned long long (*uk_store_get_ulonglong_func_t)(void);
-typedef void (*uk_store_get_void_func_t)(void);
-typedef void *(*uk_store_get_voidstar_func_t)(void);
+typedef __s8 (*uk_store_get_s8_func_t)(void);
+typedef __u8 (*uk_store_get_u8_func_t)(void);
+typedef __s16 (*uk_store_get_s16_func_t)(void);
+typedef __u16 (*uk_store_get_u16_func_t)(void);
+typedef __s32 (*uk_store_get_s32_func_t)(void);
+typedef __u32 (*uk_store_get_u32_func_t)(void);
+typedef __s64 (*uk_store_get_s64_func_t)(void);
+typedef __u64 (*uk_store_get_u64_func_t)(void);
+typedef __uptr (*uk_store_get_uptr_func_t)(void);
 
 /* Setter definitions */
-typedef void (*uk_store_set_bool_func_t)(_Bool);
-typedef void (*uk_store_set_int_func_t)(int);
-typedef void (*uk_store_set_short_func_t)(short);
-typedef void (*uk_store_set_char_func_t)(char);
-typedef void (*uk_store_set_long_func_t)(long);
-typedef void (*uk_store_set_longlong_func_t)(long long);
-typedef void (*uk_store_set_float_func_t)(float);
-typedef void (*uk_store_set_double_func_t)(double);
-typedef void (*uk_store_set_uint_func_t)(unsigned int);
-typedef void (*uk_store_set_ushort_func_t)(unsigned short);
-typedef void (*uk_store_set_uchar_func_t)(unsigned char);
-typedef void (*uk_store_set_ulong_func_t)(unsigned long);
-typedef void (*uk_store_set_ulonglong_func_t)(unsigned long long);
-typedef void (*uk_store_set_void_func_t)(void);
-typedef void (*uk_store_set_voidstar_func_t)(void *);
+typedef void (*uk_store_set_s8_func_t)(__s8);
+typedef void (*uk_store_set_u8_func_t)(__u8);
+typedef void (*uk_store_set_s16_func_t)(__s16);
+typedef void (*uk_store_set_u16_func_t)(__u16);
+typedef void (*uk_store_set_s32_func_t)(__s32);
+typedef void (*uk_store_set_u32_func_t)(__u32);
+typedef void (*uk_store_set_s64_func_t)(__s64);
+typedef void (*uk_store_set_u64_func_t)(__u64);
+typedef void (*uk_store_set_uptr_func_t)(__uptr);
 
 
 /* Stores functions and their types and a node connection to the tree */
 struct uk_store_entry {
 	enum uk_store_entry_type get_type, set_type;
 	union {
-		uk_store_get_bool_func_t      _bool;
-		uk_store_get_int_func_t       _int;
-		uk_store_get_short_func_t     _short;
-		uk_store_get_char_func_t      _char;
-		uk_store_get_long_func_t      _long;
-		uk_store_get_longlong_func_t  _longlong;
-		uk_store_get_float_func_t     _float;
-		uk_store_get_double_func_t    _double;
-		uk_store_get_uint_func_t      _uint;
-		uk_store_get_ushort_func_t    _ushort;
-		uk_store_get_uchar_func_t     _uchar;
-		uk_store_get_ulong_func_t     _ulong;
-		uk_store_get_ulonglong_func_t _ulonglong;
-		uk_store_get_void_func_t      _void;
-		uk_store_get_voidstar_func_t  _voidstar;
+		uk_store_get_s8_func_t   s8;
+		uk_store_get_u8_func_t   u8;
+		uk_store_get_s16_func_t  s16;
+		uk_store_get_u16_func_t  u16;
+		uk_store_get_s32_func_t  s32;
+		uk_store_get_u32_func_t  u32;
+		uk_store_get_s64_func_t  s64;
+		uk_store_get_u64_func_t  u64;
+		uk_store_get_uptr_func_t uptr;
 	} get;
 	union {
-		uk_store_set_bool_func_t      _bool;
-		uk_store_set_int_func_t       _int;
-		uk_store_set_short_func_t     _short;
-		uk_store_set_char_func_t      _char;
-		uk_store_set_long_func_t      _long;
-		uk_store_set_longlong_func_t  _longlong;
-		uk_store_set_float_func_t     _float;
-		uk_store_set_double_func_t    _double;
-		uk_store_set_uint_func_t      _uint;
-		uk_store_set_ushort_func_t    _ushort;
-		uk_store_set_uchar_func_t     _uchar;
-		uk_store_set_ulong_func_t     _ulong;
-		uk_store_set_ulonglong_func_t _ulonglong;
-		uk_store_set_void_func_t      _void;
-		uk_store_set_voidstar_func_t  _voidstar;
+		uk_store_set_s8_func_t   s8;
+		uk_store_set_u8_func_t   u8;
+		uk_store_set_s16_func_t  s16;
+		uk_store_set_u16_func_t  u16;
+		uk_store_set_s32_func_t  s32;
+		uk_store_set_u32_func_t  u32;
+		uk_store_set_s64_func_t  s64;
+		uk_store_set_u64_func_t  u64;
+		uk_store_set_uptr_func_t uptr;
 	} set;
+	char *entry_name;
 	struct uk_tree_node node;
 };
 
@@ -139,7 +115,7 @@ extern struct uk_store_entry *uk_store_libs;
 /* Used for registration and nothing else */
 static struct uk_store_entry uk_store_section_head __unused;
 static const struct uk_store_entry uk_store_section_entry __unused = {
-	.get_type = UK_STORE_NONE
+	.get_type = UK_STORE_ENT_NONE
 };
 
 /**
@@ -164,13 +140,15 @@ static const struct uk_store_entry uk_store_section_entry __unused = {
  * Registers an entry in the section and initializes the tree structure.
  *
  * @param offset the offset where to register
+ * @param name the name of the entry to save (char *)
  */
-#define UK_STORE_STATIC_REGISTER_INIT(offset)				\
+#define UK_STORE_STATIC_REGISTER_INIT(offset, name)			\
 	do {								\
 		struct uk_store_entry *to_reg =				\
 			(uk_store_libs + (offset));			\
-		to_reg->get_type = to_reg->set_type = UK_STORE_NONE;	\
-		UK_TREE_ROOT_INIT(&to_reg->node);			\
+		to_reg->get_type = to_reg->set_type = UK_STORE_ENT_NONE;\
+		to_reg->entry_name = strdup(name);			\
+		UK_TREE_NODE_INIT(&to_reg->node);			\
 		__UK_STORE_ENTRY_REG(uk_store_section_entry);		\
 		__UK_STORE_ENTRY_REG_H(uk_store_section_head);		\
 	} while (0)
@@ -197,26 +175,12 @@ static inline void
 uk_store_init_entry(struct uk_store_entry *entry)
 {
 	memset(entry, 0, sizeof(*entry));
-	entry->get_type = entry->set_type = UK_STORE_NONE;
+	entry->get_type = entry->set_type = UK_STORE_ENT_NONE;
+	UK_TREE_NODE_INIT(&entry->node);
 }
 
 /**
- * Initializes an empty entry and adds it to the place next pointers
- *
- * @param place the place where to add the new entry
- * @param entry the entry to initialize and add
- * @return 0 on success or < 0 on fail
- */
-static inline int
-uk_store_add_and_init_entry(struct uk_store_entry *place,
-				struct uk_store_entry *entry)
-{
-	uk_store_init_entry(entry);
-	return uk_tree_add_new(&place->node, &entry->node);
-}
-
-/**
- * Adds an entry to the place next pointers
+ * Adds an entry to the `place`'s `next` pointers
  *
  * @param place the place where to add the new entry
  * @param entry the entry to add
@@ -225,22 +189,7 @@ uk_store_add_and_init_entry(struct uk_store_entry *place,
 static inline int
 uk_store_add_entry(struct uk_store_entry *place, struct uk_store_entry *entry)
 {
-	return uk_tree_add_existing(&place->node, &entry->node);
-}
-
-
-/**
- * Removes the old entry and replaces it with the new one
- *
- * @param old the entry to remove
- * @param new the entry to add
- * @return 0 on success or < 0 on fail
- */
-static inline int
-uk_store_repl_entry(struct uk_store_entry *old, struct uk_store_entry *new)
-{
-	old->get_type = old->set_type = UK_STORE_NONE;
-	return uk_tree_replace(&old->node, &new->node);
+	return uk_tree_add(&place->node, &entry->node);
 }
 
 /**
@@ -250,10 +199,10 @@ uk_store_repl_entry(struct uk_store_entry *old, struct uk_store_entry *new)
  * @return 0 on success or < 0 on fail
  */
 static inline int
-uk_store_del_entry(struct uk_store_entry *entry)
+uk_store_del_entry(struct uk_store_entry *entry, struct uk_store_entry *parent)
 {
-	entry->get_type = entry->set_type = UK_STORE_NONE;
-	return uk_tree_del(&entry->node);
+	entry->get_type = entry->set_type = UK_STORE_ENT_NONE;
+	return uk_tree_del(&entry->node, &parent->node);
 }
 
 /**
@@ -268,9 +217,7 @@ static inline struct uk_store_entry *
 uk_store_get_entry_by_path(struct uk_store_entry *root,
 			const uint16_t *path, const uint16_t path_len)
 {
-	struct uk_tree_node *node = uk_tree_find(&root->node, path, path_len);
-
-	return node ? uk_tree_entry(node, struct uk_store_entry, node) : NULL;
+	return 0;
 }
 
 /**
@@ -287,65 +234,44 @@ uk_store_save_getter(struct uk_store_entry *entry,
 {
 	entry->get_type = type;
 	switch(entry->get_type) {
-	case UK_STORE_ENT_BOOL:
-		entry->get._bool = func;
-		break;
-
 	case UK_STORE_ENT_INT:
-		entry->get._int = func;
+	case UK_STORE_ENT_S32:
+		entry->get.s32 = func;
 		break;
 
-	case UK_STORE_ENT_SHORT:
-		entry->get._short = func;
+	case UK_STORE_ENT_S16:
+		entry->get.s16 = func;
 		break;
 
 	case UK_STORE_ENT_CHAR:
-		entry->get._char = func;
+	case UK_STORE_ENT_S8:
+		entry->get.s8 = func;
 		break;
 
-	case UK_STORE_ENT_LONG:
-		entry->get._long = func;
+	case UK_STORE_ENT_S64:
+		entry->get.s64 = func;
 		break;
 
-	case UK_STORE_ENT_LONGLONG:
-		entry->get._longlong = func;
+	case UK_STORE_ENT_U32:
+		entry->get.u32 = func;
 		break;
 
-	case UK_STORE_ENT_FLOAT:
-		entry->get._float = func;
+	case UK_STORE_ENT_U16:
+		entry->get.u16 = func;
 		break;
 
-	case UK_STORE_ENT_DOUBLE:
-		entry->get._double = func;
+	case UK_STORE_ENT_U8:
+		entry->get.u8 = func;
 		break;
 
-	case UK_STORE_ENT_UINT:
-		entry->get._uint = func;
-		break;
-	
-	case UK_STORE_ENT_USHORT:
-		entry->get._ushort = func;
-		break;
-	
-	case UK_STORE_ENT_UCHAR:
-		entry->get._uchar = func;
-		break;
-	
-	case UK_STORE_ENT_ULONG:
-		entry->get._ulong = func;
-		break;
-	
-	case UK_STORE_ENT_ULONGLONG:
-		entry->get._ulonglong = func;
+	case UK_STORE_ENT_U64:
+		entry->get.u64 = func;
 		break;
 
-	case UK_STORE_ENT_VOID:
-		entry->get._void = func;
+	case UK_STORE_ENT_UPTR:
+	case UK_STORE_ENT_CHARP:
+		entry->get.uptr = func;
 		break;
-
-	case UK_STORE_ENT_VOIDSTAR:
-		entry->get._voidstar = func;
-		break;	
 	default:
 		return -EINVAL;
 	}
@@ -366,64 +292,43 @@ uk_store_save_setter(struct uk_store_entry *entry,
 {
 	entry->set_type = type;
 	switch(entry->set_type) {
-	case UK_STORE_ENT_BOOL:
-		entry->set._bool = func;
-		break;
-
 	case UK_STORE_ENT_INT:
-		entry->set._int = func;
+	case UK_STORE_ENT_S32:
+		entry->set.s32 = func;
 		break;
 
-	case UK_STORE_ENT_SHORT:
-		entry->set._short = func;
+	case UK_STORE_ENT_S16:
+		entry->set.s16 = func;
 		break;
 
 	case UK_STORE_ENT_CHAR:
-		entry->set._char = func;
+	case UK_STORE_ENT_S8:
+		entry->set.s8 = func;
 		break;
 
-	case UK_STORE_ENT_LONG:
-		entry->set._long = func;
+	case UK_STORE_ENT_S64:
+		entry->set.s64 = func;
 		break;
 
-	case UK_STORE_ENT_LONGLONG:
-		entry->set._longlong = func;
+	case UK_STORE_ENT_U32:
+		entry->set.u32 = func;
 		break;
 
-	case UK_STORE_ENT_FLOAT:
-		entry->set._float = func;
+	case UK_STORE_ENT_U16:
+		entry->set.u16 = func;
 		break;
 
-	case UK_STORE_ENT_DOUBLE:
-		entry->set._double = func;
+	case UK_STORE_ENT_U8:
+		entry->set.u8 = func;
 		break;
 
-	case UK_STORE_ENT_UINT:
-		entry->set._uint = func;
-		break;
-	
-	case UK_STORE_ENT_USHORT:
-		entry->set._ushort = func;
-		break;
-	
-	case UK_STORE_ENT_UCHAR:
-		entry->set._uchar = func;
-		break;
-	
-	case UK_STORE_ENT_ULONG:
-		entry->set._ulong = func;
-		break;
-	
-	case UK_STORE_ENT_ULONGLONG:
-		entry->set._ulonglong = func;
+	case UK_STORE_ENT_U64:
+		entry->set.u64 = func;
 		break;
 
-	case UK_STORE_ENT_VOID:
-		entry->set._void = func;
-		break;
-
-	case UK_STORE_ENT_VOIDSTAR:
-		entry->set._voidstar = func;
+	case UK_STORE_ENT_UPTR:
+	case UK_STORE_ENT_CHARP:
+		entry->set.uptr = func;
 		break;	
 	default:
 		return -EINVAL;
@@ -444,20 +349,6 @@ uk_store_is_file(struct uk_store_entry *entry)
 }
 
 /**
- * Checks if an entry is a folder (not leaf)
- *
- * @param entry the entry to check
- * @return 1 if file, 0 if not, or < 0 on fail
- */
-static inline int
-uk_store_is_folder(struct uk_store_entry *entry)
-{
-	int ret = uk_store_is_file(entry);
-
-	return (ret < 0) ? ret : !ret;
-}
-
-/**
  * Gets the value returned by the saved function and puts it in `out`
  *
  * @param entry the entry to use
@@ -468,64 +359,43 @@ static inline int
 uk_store_get_value(struct uk_store_entry *entry, void *out)
 {
 	switch(entry->get_type) {
-	case UK_STORE_ENT_BOOL:
-		*((_Bool *) out) = entry->get._bool();
-		break;
-
 	case UK_STORE_ENT_INT:
-		*((int *) out) = entry->get._int();
+	case UK_STORE_ENT_S32:
+		*((__s32 *) out) = entry->get.s32();
 		break;
 
-	case UK_STORE_ENT_SHORT:
-		*((short *) out) = entry->get._short();
+	case UK_STORE_ENT_S16:
+		*((__s16 *) out) = entry->get.s16();
 		break;
 
 	case UK_STORE_ENT_CHAR:
-		*((char *) out) = entry->get._char();
+	case UK_STORE_ENT_S8:
+		*((__s8 *) out) = entry->get.s8();
 		break;
 
-	case UK_STORE_ENT_LONG:
-		*((long *) out) = entry->get._long();
+	case UK_STORE_ENT_S64:
+		*((__s64 *) out) = entry->get.s64();
 		break;
 
-	case UK_STORE_ENT_LONGLONG:
-		*((long long *) out) = entry->get._longlong();
+	case UK_STORE_ENT_U32:
+		*((__u32 *) out) = entry->get.u32();
 		break;
 
-	case UK_STORE_ENT_FLOAT:
-		*((float *) out) = entry->get._float();
+	case UK_STORE_ENT_U16:
+		*((__u16 *) out) = entry->get.u16();
 		break;
 
-	case UK_STORE_ENT_DOUBLE:
-		*((double *) out) = entry->get._double();
+	case UK_STORE_ENT_U8:
+		*((__u8 *) out) = entry->get.u8();
 		break;
 
-	case UK_STORE_ENT_UINT:
-		*((unsigned int *) out) = entry->get._uint();
-		break;
-	
-	case UK_STORE_ENT_USHORT:
-		*((unsigned short *) out) = entry->get._ushort();
-		break;	
-	
-	case UK_STORE_ENT_UCHAR:
-		*((unsigned char *) out) = entry->get._uchar();
-		break;	
-	
-	case UK_STORE_ENT_ULONG:
-		*((unsigned long *) out) = entry->get._ulong();
-		break;	
-	
-	case UK_STORE_ENT_ULONGLONG:
-		*((unsigned long long *) out) = entry->get._ulonglong();
-		break;	
-	
-	case UK_STORE_ENT_VOID:
-		entry->get._void();
+	case UK_STORE_ENT_U64:
+		*((__u64 *) out) = entry->get.u64();
 		break;
 
-	case UK_STORE_ENT_VOIDSTAR:
-		*((void  **) out) = entry->get._voidstar();
+	case UK_STORE_ENT_UPTR:
+	case UK_STORE_ENT_CHARP:
+		*((__uptr *) out) = entry->get.uptr();
 		break;
 	default:
 		return -EINVAL;
@@ -545,64 +415,43 @@ static inline int
 uk_store_set_value(struct uk_store_entry *entry, void *in)
 {
 	switch(entry->set_type) {
-	case UK_STORE_ENT_BOOL:
-		entry->set._bool(*((_Bool *) in));
-		break;
-
 	case UK_STORE_ENT_INT:
-		entry->set._int(*((int *) in));
+	case UK_STORE_ENT_S32:
+		entry->set.s32(*((__s32 *) in));
 		break;
 
-	case UK_STORE_ENT_SHORT:
-		entry->set._short(*((short *) in));
+	case UK_STORE_ENT_S16:
+		entry->set.s16(*((__s16 *) in));
 		break;
 
 	case UK_STORE_ENT_CHAR:
-		entry->set._char(*((char *) in));
+	case UK_STORE_ENT_S8:
+		entry->set.s8(*((__s8 *) in));
 		break;
 
-	case UK_STORE_ENT_LONG:
-		entry->set._long(*((long *) in));
+	case UK_STORE_ENT_S64:
+		entry->set.s64(*((__s64 *) in));
 		break;
 
-	case UK_STORE_ENT_LONGLONG:
-		entry->set._longlong(*((long long *) in));
+	case UK_STORE_ENT_U32:
+		entry->set.u32(*((__u32 *) in));
 		break;
 
-	case UK_STORE_ENT_FLOAT:
-		entry->set._float(*((float *) in));
+	case UK_STORE_ENT_U16:
+		entry->set.u16(*((__u16 *) in));
 		break;
 
-	case UK_STORE_ENT_DOUBLE:
-		entry->set._double(*((double *) in));
+	case UK_STORE_ENT_U8:
+		entry->set.u8(*((__u8 *) in));
 		break;
 
-	case UK_STORE_ENT_UINT:
-		entry->set._uint(*((unsigned int *) in));
-		break;
-	
-	case UK_STORE_ENT_USHORT:
-		entry->set._bool(*((unsigned short *) in));
-		break;
-	
-	case UK_STORE_ENT_UCHAR:
-		entry->set._uchar(*((unsigned char *) in));
-		break;
-	
-	case UK_STORE_ENT_ULONG:
-		entry->set._ulong(*((unsigned long *) in));
-		break;
-	
-	case UK_STORE_ENT_ULONGLONG:
-		entry->set._ulonglong(*((unsigned long long *) in));
+	case UK_STORE_ENT_U64:
+		entry->set.u64(*((__u64 *) in));
 		break;
 
-	case UK_STORE_ENT_VOID:
-		entry->set._void();
-		break;
-
-	case UK_STORE_ENT_VOIDSTAR:
-		entry->set._voidstar(in);
+	case UK_STORE_ENT_UPTR:
+	case UK_STORE_ENT_CHARP:
+		entry->set.uptr(*((__uptr *) in));
 		break;
 	default:
 		return -EINVAL;
