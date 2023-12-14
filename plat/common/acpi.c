0179d1e84b4df20e379a746694716743e3c5afa2
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2023-12-14 17:01:35 +0200

    plat/common: Add the cool new university name
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

--- plat/common/acpi.c
+++ plat/common/acpi.c
@@ -3,7 +3,7 @@
  * Authors: Cristian Vijelie <cristianvijelie@gmail.com>
  *          Sergiu Moga <sergiu.moga@protonmail.com>
  *
- * Copyright (c) 2023, University POLITEHNICA of Bucharest. All rights reserved.
+ * Copyright (c) 2023, National University for Technology and Science of Bucharest. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
